db77b378362e1818d8e8d300fd178f91f7049f48
joaomdp <joaomanoeldpereira@gmail.com>
2022-05-30 20:44:53 -0300

    Atualização_30/05/2022

---
+++ trabalhos_ulbra_2022_01/Introdução a computação/aula14/atividades_js/exer_01/arquivo.js
@@ -0,0 +1,38 @@
+//EXERCICIO 1
+var numero = 10;
+console.log("" + numero);
+console.log(typeof ('number'));
+
+//EXERCICIO 5 
+var mediaFinal = 0;
+
+var avaliacaoParcial = 1;
+console.log("Avaliação parcial " + avaliacaoParcial);
+
+var avaliacaoParcial2 = 2;
+console.log("Avaliação parcial " + avaliacaoParcial2);
+
+var ativSemestral = 5;
+console.log("Avaliação semestral " + ativSemestral);
+
+mediaFinal = avaliacaoParcial + avaliacaoParcial2 + ativSemestral;
+
+console.log("Media final " + mediaFinal);
+
+//EXERCICIO 6 
+var media = 0;
+
+var ap1 = 1.5;
+console.log("Valor de ap1 " + ap1);
+
+var ap2 = 2.5;
+console.log("Valor de ap2 " + ap2);
+
+var as = 6;
+console.log("Valor de avaliação semestral " + as);
+
+media = ap1 + ap2 + as;
+
+console.log("Media final " + media);
+
+
---
+++ trabalhos_ulbra_2022_01/Introdução a computação/aula14/atividades_js/exer_07/script.js
@@ -0,0 +1,8 @@
+var idade=0;
+var idade = prompt('qual sua idade? ');
+
+if('idade<17'){
+    alert('Você é menor de idade');
+}else{
+    alert('Você é maior de idade');
+}
