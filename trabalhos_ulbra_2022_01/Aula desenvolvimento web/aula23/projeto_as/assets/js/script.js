a5dcdb122300c244d3ba6b67d6a39ba1a99ba131
joaomdp <joaomanoeldpereira@gmail.com>
2022-08-01 14:38:37 -0300

    Atualização_01/08/2022

--- trabalhos_ulbra_2022_01/Aula desenvolvimento web/aula23/projeto_as/assets/js/script.js
+++ trabalhos_ulbra_2022_01/Aula desenvolvimento web/aula23/projeto_as/assets/js/script.js
@@ -5,9 +5,10 @@ $(document).ready(()=>{
 
     function clearClass(params) {
         $('#menuHome').removeClass('active')
-        $('#menuSobre').removeClass('active')
+        $('#menuClube').removeClass('active')
         $('#menuProduto').removeClass('active')
-        $('#menuContato').removeClass('active')
+        $('#menuSocio').removeClass('active')
+        $('#menuCasa').removeClass('active')
     }
 
     $('#menuHome').click(()=>{
@@ -16,10 +17,10 @@ $(document).ready(()=>{
         $('section').load('pages/home.html')
     })
 
-    $('#menuSobre').click(()=>{
+    $('#menuClube').click(()=>{
         clearClass()
-        $('#menuSobre').removeClass('active')
-        $('section').load('pages/sobre.html')
+        $('#menuClube').removeClass('active')
+        $('section').load('pages/clube.html')
     })
     
     $('#menuProduto').click(()=>{
@@ -28,9 +29,50 @@ $(document).ready(()=>{
         $('section').load('pages/produtos.html')
     })
     
-    $('#menuContato').click(()=>{
+    $('#menuSocio').click(()=>{
         clearClass()
-        $('#menuContato').removeClass('active')
-        $('section').load('pages/contato.html')
+        $('#menuSocio').removeClass('active')
+        $('section').load('pages/socio.html')
     })
+
+    $('#menuCasa').click(()=>{
+        clearClass()
+        $('#menuCasa').removeClass('active')
+        $('section').load('pages/casa.html')
+    })
+    
 })
+
+// FUNÇÃO VALIDAÇÃO
+$("#FormValidation").validate({
+    rules:{
+        name:{
+            minlenght:2
+        },
+        email:{
+            email: true
+        },
+        phone:{
+            Number:true,
+            minlenght:10,
+            maxlenght:10
+        },
+    },
+    messages:{
+        name:{
+            required: "Por favor, digite seu nome completo",
+            minlenght: "Seu nome deve ter no mínimo duas letras"
+        },
+        email:"Por favor, digite seu email",
+        phone:"Por favor, digite seu número de telefone",
+        sexo:"Por favor, insira seu sexo",
+        formMessage:"Por favor, digite uma observação"
+    },
+    submitHandler: function(form) {
+      form.submit();
+    }
+   });
+
+function redirecionamneto() {
+    window.location.replace("../index.html")
+}
