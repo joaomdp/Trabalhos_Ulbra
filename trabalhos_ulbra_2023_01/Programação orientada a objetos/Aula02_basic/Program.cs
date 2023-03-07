0371d9fa6a708e07c4df837235e05dbb6da89edd
joaomdp <joaomanoeldpereira@gmail.com>
2023-03-07 19:17:14 -0300

    Atualização_2023/03/07

---
+++ trabalhos_ulbra_2023_01/Programação orientada a objetos/Aula02_basic/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Aula02_basic;
+
+Console.WriteLine("Hello, World!");
+
+//tipos de variaveis
+int idade = 25;
+string nome = "João";
+bool possuiCarteira = true;
+double altura = 1.75;
+decimal valorUnit = 22;
+
+//instanciar um objeto
+
+Garrafa garrafaDoCassio = new Garrafa(1234,"cinza",2000,"garrafa mais bonita");
+
+Console.WriteLine(garrafaDoCassio.AnoFabricacao + "" + garrafaDoCassio.Cor);
+garrafaDoCassio.Cor="Tricolor";
+
+Garrafa garrafaDoFelipe = new Garrafa(2334,"azul",3214,"garrafa nem tao bonita");
+
+Console.WriteLine(garrafaDoFelipe.AnoFabricacao);
+
+
+
+
+
---
+++ trabalhos_ulbra_2023_01/Programação orientada a objetos/Aula02_pratique/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Pratique 01
+Console.WriteLine("Digite um numero");
+var numero = Convert.ToInt32(Console.ReadLine());
+if (numero%2==0)
+{
+    Console.WriteLine("O numero"+ numero+" é par");
+}else{
+    Console.WriteLine("O numero"+ numero+" é impar");
+}
