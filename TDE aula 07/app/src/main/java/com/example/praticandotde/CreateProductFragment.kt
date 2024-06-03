7ef0362d256b57a9db60a63e416756ff562ef14e
joaomdp <joaomanoeldpereira@gmail.com>
2024-06-03 20:25:21 -0300

    Atualização_03_06

---
+++ TDE aula 07/app/src/main/java/com/example/praticandotde/CreateProductFragment.kt
@@ -0,0 +1,24 @@
+package com.example.praticandotde
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateProductFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_product, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (requireActivity() as AppCompatActivity).configureToolbar("create Product", false)
+    }
+}
